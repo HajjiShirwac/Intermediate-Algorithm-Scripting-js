f13752edd262eefd90c6b3f7dca847042ee1397b
hajjishirwac <shirwachajji@gmail.com>
2022-09-11 15:06:11 +0300

    Seek and Destroy

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -33,3 +33,24 @@ function sumAll(arr) {
   }
   
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+  ///   Seek and Destroy
+
+  function destroyer(arr) {
+    const valsToRemove = Object.values(arguments).slice(1);
+    const filteredArray = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      let removeElement = false;
+      for (let j = 0; j < valsToRemove.length; j++) {
+        if (arr[i] === valsToRemove[j]) {
+          removeElement = true;
+        }
+      }
+      if (!removeElement) {
+        filteredArray.push(arr[i]);
+      }
+    }
+    return filteredArray;
+  }  
