556a2ae55aaeda86d136e242cb51977089a42fc9
hajjishirwac <shirwachajji@gmail.com>
2022-09-12 15:31:27 +0300

    DNA Pairing

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -139,3 +139,37 @@ function whatIsInAName(collection, source) {
   
   // test here
   myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+
+  //    //  //  // DNA Pairing
+
+  function pairElement(str) {
+    // Function to match each character with the base pair
+    let matchWithBasePair = function(char, pairedArray) {
+      switch (char) {
+        case "A":
+          pairedArray.push(["A", "T"]);
+          break;
+        case "T":
+          pairedArray.push(["T", "A"]);
+          break;
+        case "C":
+          pairedArray.push(["C", "G"]);
+          break;
+        case "G":
+          pairedArray.push(["G", "C"]);
+          break;
+      }
+    };
+  
+    // Find pair for every character in the string
+    const paired = [];
+    for (let i = 0; i < str.length; i++) {
+      matchWithBasePair(str[i], paired);
+    }
+  
+    return paired;
+  }
+  
+  // test here
+  pairElement("GCG");
