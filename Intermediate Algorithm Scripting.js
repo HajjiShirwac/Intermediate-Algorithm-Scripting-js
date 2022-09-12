3833ae01e63c0899e43a74088ca961f596977f56
hajjishirwac <shirwachajji@gmail.com>
2022-09-12 14:25:27 +0300

    Pig Latin

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -101,3 +101,19 @@ function whatIsInAName(collection, source) {
   
   // test here
   spinalCase("This Is Spinal Tap");
+
+
+  //    //      // Pig Latin
+
+  function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("consonant");
