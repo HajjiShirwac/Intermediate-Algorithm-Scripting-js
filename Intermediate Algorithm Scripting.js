e9ef430e9d786d51ac158dc27ccf98aa49ef2a6e
hajjishirwac <shirwachajji@gmail.com>
2022-09-13 14:33:12 +0300

    Sum All Primes

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -277,3 +277,24 @@ function whatIsInAName(collection, source) {
   
   // test here
   sumFibs(4);
+
+
+    //  //  //  /// Sum All Primes
+    function sumPrimes(num) {
+        // Helper function to check primality
+        function isPrime(num) {
+          for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+              return false;
+          }
+          return true;
+        }
+      
+        // Check all numbers for primality
+        let sum = 0;
+        for (let i = 2; i <= num; i++) {
+          if (isPrime(i))
+            sum += i;
+        }
+        return sum;
+      }
