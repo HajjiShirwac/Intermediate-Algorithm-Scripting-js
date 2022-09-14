12e57de6a6fbf16c1acf7f013c9f0a38003362ff
hajjishirwac <shirwachajji@gmail.com>
2022-09-14 14:02:09 +0300

    Steamroller

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -339,3 +339,25 @@ function whatIsInAName(collection, source) {
       dropElements([1, 2, 3, 4], function(n) {
         return n >= 3;
       });
+
+
+      /// ///   /// Steamroller
+
+      function steamrollArray(arr) {
+        const flattenedArray = [];
+        // Loop over array contents
+        for (let i = 0; i < arr.length; i++) {
+          if (Array.isArray(arr[i])) {
+            // Recursively flatten entries that are arrays
+            //  and push into the flattenedArray
+            flattenedArray.push(...steamrollArray(arr[i]));
+          } else {
+            // Copy contents that are not arrays
+            flattenedArray.push(arr[i]);
+          }
+        }
+        return flattenedArray;
+      };
+      
+      // test here
+      steamrollArray([1, [2], [3, [[4]]]]);
