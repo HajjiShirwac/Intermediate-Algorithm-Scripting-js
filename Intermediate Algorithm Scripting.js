139d3f3aa136fab5172a1f8996e81d818015ac6c
hajjishirwac <shirwachajji@gmail.com>
2022-09-14 14:00:00 +0300

    Drop it

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -327,3 +327,15 @@ function whatIsInAName(collection, source) {
       }
       
       smallestCommons([1, 5]);
+      //  //  //Drop it
+      function dropElements(arr, func) {
+        while (arr.length > 0 && !func(arr[0])) {
+          arr.shift();
+        }
+        return arr;
+      }
+      
+      // test here
+      dropElements([1, 2, 3, 4], function(n) {
+        return n >= 3;
+      });
