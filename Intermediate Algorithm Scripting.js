6d45228b043e014d6ce922b291a99c7130c5f2e6
hajjishirwac <shirwachajji@gmail.com>
2022-09-15 14:51:18 +0300

    Map the Debris

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -452,3 +452,28 @@ function whatIsInAName(collection, source) {
       
       const bob = new Person("Bob Ross");
       console.log(bob.getFullName());
+//  //  // Map the Debris
+
+      function orbitalPeriod(arr) {
+        const GM = 398600.4418;
+        const earthRadius = 6367.4447;
+        const a = 2 * Math.PI;
+        const newArr = [];
+      
+        const getOrbPeriod = function(obj) {
+          const c = Math.pow(earthRadius + obj.avgAlt, 3);
+          const b = Math.sqrt(c / GM);
+          const orbPeriod = Math.round(a * b);
+          // create new object
+          return {name: obj.name, orbitalPeriod: orbPeriod};
+        };
+      
+        for (let elem in arr) {
+          newArr.push(getOrbPeriod(arr[elem]));
+        }
+      
+        return newArr;
+      }
+      
+      // test here
+      orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
